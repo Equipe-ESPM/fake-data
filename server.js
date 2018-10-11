be7e63b1c5f94f0d4abd67179e66e8d9deb84540
Icaro Gavazza <icarodgl@gmail.com>
2018-10-11 13:23:23 -0300

    Banco em memoria para usar no heroku

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+//load dos bancos
+var busca = require("./db.busca.json");
+var usuario = require("./db.usuario.json");
+//tentando usar in memory
+var dbInMemory = {
+  busca: busca,
+  usuario: usuario
+};
+const router = jsonServer.router(dbInMemory);
+
 var portDefault = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
